749196e8b46fa4b56466db0bda8cb14cced7e299
son <ourendpoint@gmail.com>
2021-09-22 21:29:06 +0900

    update: tutorial_6

--- tutorial_6/Source/tutorial_6/tutorial_6GameModeBase.cpp
+++ tutorial_6/Source/tutorial_6/tutorial_6GameModeBase.cpp
@@ -2,4 +2,28 @@
 
 
 #include "tutorial_6GameModeBase.h"
+#include <Blueprint/UserWidget.h>
 
+void Atutorial_6GameModeBase::BeginPlay()
+{
+	Super::BeginPlay();
+	ChangeMenuWidget(StartingWidgetClass);
+}
+
+void Atutorial_6GameModeBase::ChangeMenuWidget(TSubclassOf<UUserWidget> NewWidgetClass)
+{
+	if (CurrentWidget != nullptr)
+	{
+		CurrentWidget->RemoveFromViewport();
+		CurrentWidget = nullptr;
+	}
+
+	if (NewWidgetClass != nullptr)
+	{
+		CurrentWidget = CreateWidget(GetWorld(), NewWidgetClass);
+		if (CurrentWidget != nullptr)
+		{
+			CurrentWidget->AddToViewport();
+		}
+	}
+}
